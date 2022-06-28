830ca0531a7af126e26c9fd5bd27d81072fb9840
MANJUSHA <manjusha4career@gmail.com>
2022-06-28 11:22:58 +0530

    update angular.json

--- blog/dist/out-tsc/app/bloglist/blog.model.js
+++
@@ -1,8 +0,0 @@
-export class BlogModel {
-    constructor(title, author, body) {
-        this.title = title;
-        this.author = author;
-        this.body = body;
-    }
-}
-//# sourceMappingURL=blog.model.js.map
